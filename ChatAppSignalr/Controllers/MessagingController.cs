318380a2f87008488191d371a3c71b3900a15422
sunnykachwala <sunny@convert.no>
2016-05-04 10:31:45 +0530

    First Commit

---
+++ ChatAppSignalr/Controllers/MessagingController.cs
@@ -0,0 +1,55 @@
+﻿using ChatAppSignalr.Hubs;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChatAppSignalr.Controllers
+{
+    public abstract class ApiControllerWithHub<THub> : ApiController
+        where THub : IHub
+    {
+        Lazy<IHubContext> hub = new Lazy<IHubContext>(
+            () => GlobalHost.ConnectionManager.GetHubContext<THub>()
+        );
+
+        protected IHubContext Hub
+        {
+            get { return hub.Value; }
+        }
+    }
+
+    public class MessagingController : ApiControllerWithHub<ChatHub> // ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ChatAppSignalr/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatAppSignalr.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        public void Subscribe(string UserId)
+        {
+            Groups.Add(Context.ConnectionId, UserId);
+        }
+
+        public void Unsubscribe(string UserId)
+        {
+            Groups.Remove(Context.ConnectionId, UserId);
+        }
+    }
+}
